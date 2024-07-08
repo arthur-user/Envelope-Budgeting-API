c6f98216b589232abeb062c9056464f34010e42b
arthur-user <115193136+arthur-user@users.noreply.github.com>
2024-07-08 00:12:55 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const logger = require("morgan");
+/*. Storing configuration in the environment separate from code is based on The Twelve-Factor App methodology.*/
+dotenv.config({ path: "./conf/config.env" });
+
+const envelopesRouter = require("./routes/envelopes");
+const docsRouter = require("./routes/docs");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+
+app.use("/api-docs", docsRouter);
+
+app.use("/api/v1/envelopes", envelopesRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
